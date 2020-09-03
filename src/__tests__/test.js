56b987ee4b50fbc4d534c52d8b1280525999028f
jobo322 <jose.bolanos@correounivalle.edu.co>
2020-09-03 09:53:43 -0500

    feat: add test case

---
+++ src/__tests__/test.js
@@ -0,0 +1,31 @@
+import { Matrix } from 'ml-matrix';
+import { toBeDeepCloseTo } from 'jest-matcher-deep-close-to';
+import { ngmca } from '../index';
+
+expect.extend({ toBeDeepCloseTo });
+
+describe('NMF test', () => {
+  it('use case 1', async () => {
+    let w = new Matrix([
+      [1, 2, 3],
+      [4, 5, 6],
+    ]);
+    let h = new Matrix([
+      [1, 2],
+      [3, 4],
+      [5, 6],
+    ]);
+
+    let v = w.mmul(h);
+
+    const options = {
+      maximumIteration: 100,
+      phaseRatio: 0.4,
+    };
+
+    const result = ngmca(v, 2, options);
+    const w0 = result.A;
+    const h0 = result.S;
+    expect(w0.mmul(h0).to2DArray()).toBeDeepCloseTo(v.to2DArray(), 1);
+  });
+});
