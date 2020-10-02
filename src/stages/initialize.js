4a48c8e77ad1ef9534661fa78cab26db3655b6d4
jobo322 <jose.bolanos@correounivalle.edu.co>
2020-10-02 14:08:34 -0500

    chore: trying to solve rank deficient

--- src/stages/initialize.js
+++ src/stages/initialize.js
@@ -28,54 +28,68 @@ export function initialize(originalMatrix, options = {}) {
     let sumC = result.A.sum('column');
     let positiveSumColumnA = [];
     for (let i = 0; i < sumC.length; i++) {
-      if (sumC[i] > 0) positiveSumColumnA.push(result.A.getColumn(i));
-    }
-    positiveSumColumnA = new Matrix(positiveSumColumnA);
-    //resolve the system of equation Lx = originalMatrix for x, then select just non negative values;
-    let candidateS = zeroInsteadOfNegative(
-      solve(positiveSumColumnA.transpose(), originalMatrix),
-    );
-    //set the S matrix with dimensions k x c
-    let rIndex = 0;
-    result.S = Matrix.zeros(rank, columns);
-    for (let i = 0; i < sumC.length; i++) {
-      if (sumC[i] > 0) {
-        let rowCandidate = candidateS.getRow(rIndex++);
-        for (let j = 0; j < columns; j++) {
-          result.S.set(i, j, rowCandidate[j]);
+      while (sumC[i] === 0) {
+        sumC[i] = 0;
+        for (let j = 0; j < rows; j++) {
+          result.A.set(j, i, randGenerator());
+          sumC[i] += result.A.get(j, i);
         }
       }
     }
-    console.log('result', result.S)
+
+    //resolve the system of equation Lx = D for x, then select just non negative values;
+    result.S = zeroInsteadOfNegative(solve(result.A, originalMatrix));
+
+    //set the S matrix with dimensions k x c
+    // console.log('sumC', sumC);
+    // let rIndex = 0;
+    // result.S = Matrix.zeros(rank, columns);
+    // for (let i = 0; i < sumC.length; i++) {
+    //   if (sumC[i] > 0) {
+    //     let rowCandidate = candidateS.getRow(rIndex++);
+    //     for (let j = 0; j < columns; j++) {
+    //       result.S.set(i, j, rowCandidate[j]);
+    //     }
+    //   }
+    // }
+    console.log('result S', result.S);
     //select rows with positive sum by row
     let sumR = result.S.sum('row');
+    let positiveSumRowIndexS = [];
     let positiveSumRowS = [];
     for (let i = 0; i < sumR.length; i++) {
-      if (sumR[i] > 0) positiveSumRowS.push(result.S.getRow(i));
-    }
-
-    positiveSumRowS = new Matrix(positiveSumRowS);
-
-    // solve the system of linear equation xY = S
-    console.log(positiveSumRowS.transpose(), originalMatrix.transpose())
-    let candidateA = solve(
-      positiveSumRowS.transpose(),
-      originalMatrix.transpose(),
-    );
-
-    candidateA = zeroInsteadOfNegative(candidateA.transpose());
-
-    let cIndex = 0;
-    result.A = Matrix.zeros(rows, rank);
-    for (let i = 0; i < sumR.length; i++) {
       if (sumR[i] > 0) {
-        let colCandidate = candidateA.getColumn(cIndex++);
-        for (let j = 0; j < rows; j++) {
-          result.A.set(j, i, colCandidate[j]);
-        }
+        positiveSumRowIndexS.push(i);
+        positiveSumRowS.push(result.S.getRow(i));
       }
     }
 
+    positiveSumRowS = Matrix.checkMatrix(positiveSumRowS);
+    let tempOriginalMatrixTransposed = new Matrix(
+      columns,
+      positiveSumRowIndexS.length,
+    );
+
+    for (let r = 0; r < columns; r++) {
+      for (let c = 0; c < positiveSumRowIndexS.length; c++) {
+        tempOriginalMatrixTransposed.set(
+          r,
+          c,
+          originalMatrix.get(positiveSumRowIndexS[c], r),
+        );
+      }
+    }
+
+    console.log('before solve', positiveSumRowS, tempOriginalMatrixTransposed)
+    // solve the system of linear equation xL = D for x.
+    let candidateA = solve(
+      positiveSumRowS.transpose(),
+      tempOriginalMatrixTransposed,
+    );
+
+    result.A = zeroInsteadOfNegative(candidateA.transpose());
+
+    result = checkMatrixS(result, originalMatrix);
     let prevS = result.S.clone();
     result.S = updateMatrixS(result.A, result.S, originalMatrix, 0, {
       maxFBIteration,
