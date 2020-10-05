95e0a3c14405a545948f16164a1917ea6aeac30f
jobo322 <jose.bolanos@correounivalle.edu.co>
2020-10-04 21:46:50 -0500

    fix: initialization

--- src/stages/initialize.js
+++ src/stages/initialize.js
@@ -19,14 +19,12 @@ export function initialize(originalMatrix, options = {}) {
 
   let result = {};
   let rows = originalMatrix.rows;
-  let columns = originalMatrix.columns;
 
   result.A = Matrix.rand(rows, rank, { random: randGenerator });
 
   for (let iter = 0; iter < maxInitFBIteration; iter++) {
     //select columns with sum positive from A
     let sumC = result.A.sum('column');
-    let positiveSumColumnA = [];
     for (let i = 0; i < sumC.length; i++) {
       while (sumC[i] === 0) {
         sumC[i] = 0;
@@ -40,19 +38,6 @@ export function initialize(originalMatrix, options = {}) {
     //resolve the system of equation Lx = D for x, then select just non negative values;
     result.S = zeroInsteadOfNegative(solve(result.A, originalMatrix));
 
-    //set the S matrix with dimensions k x c
-    // console.log('sumC', sumC);
-    // let rIndex = 0;
-    // result.S = Matrix.zeros(rank, columns);
-    // for (let i = 0; i < sumC.length; i++) {
-    //   if (sumC[i] > 0) {
-    //     let rowCandidate = candidateS.getRow(rIndex++);
-    //     for (let j = 0; j < columns; j++) {
-    //       result.S.set(i, j, rowCandidate[j]);
-    //     }
-    //   }
-    // }
-    console.log('result S', result.S);
     //select rows with positive sum by row
     let sumR = result.S.sum('row');
     let positiveSumRowIndexS = [];
@@ -65,37 +50,29 @@ export function initialize(originalMatrix, options = {}) {
     }
 
     positiveSumRowS = Matrix.checkMatrix(positiveSumRowS);
-    let tempOriginalMatrixTransposed = new Matrix(
-      columns,
-      positiveSumRowIndexS.length,
+
+    // solve the system of linear equation xL = D for x. knowing that D/L = (L'\D')'.
+    let candidateA = zeroInsteadOfNegative(
+      solve(positiveSumRowS.transpose(), originalMatrix.transpose()),
     );
 
-    for (let r = 0; r < columns; r++) {
-      for (let c = 0; c < positiveSumRowIndexS.length; c++) {
-        tempOriginalMatrixTransposed.set(
-          r,
-          c,
-          originalMatrix.get(positiveSumRowIndexS[c], r),
-        );
+    //then, set the columns of A with an index equal to the row index with sum > 0 into S
+    //this step complete the last transpose of D/L = (L'\D')'.
+    for (let i = 0; i < positiveSumRowIndexS.length; i++) {
+      let colCandidate = candidateA.getRow(i);
+      for (let j = 0; j < rows; j++) {
+        result.A.set(j, positiveSumRowIndexS[i], colCandidate[j]);
       }
     }
 
-    console.log('before solve', positiveSumRowS, tempOriginalMatrixTransposed)
-    // solve the system of linear equation xL = D for x.
-    let candidateA = solve(
-      positiveSumRowS.transpose(),
-      tempOriginalMatrixTransposed,
-    );
-
-    result.A = zeroInsteadOfNegative(candidateA.transpose());
-
-    result = checkMatrixS(result, originalMatrix);
     let prevS = result.S.clone();
     result.S = updateMatrixS(result.A, result.S, originalMatrix, 0, {
       maxFBIteration,
       toleranceFB,
     });
+
     result = checkMatrixS(result, originalMatrix);
+
     result.A = updateMatrixA(result.A, result.S, originalMatrix, 0, {
       maxFBIteration,
       toleranceFB,
@@ -107,7 +84,7 @@ export function initialize(originalMatrix, options = {}) {
       toleranceFBInit
     ) {
       break;
-    } //checkIt
+    }
   }
   return result;
 }
